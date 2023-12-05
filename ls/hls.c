ef36ef06a60817c6fb0c58d837a641cb7b853132
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-12-05 15:00:37 +0100

    remove style

--- ls/hls.c
+++ ls/hls.c
@@ -23,6 +23,51 @@ typedef struct
 int compareEntries(const char *a, const char *b);
 
 /* Initialiser la liste d'entrées */
+void initializeList(EntryList *list);
+
+/* Ajouter une entrée à la liste */
+void addEntry(EntryList *list, const char *name);
+
+/* Libérer la mémoire allouée pour les entrées */
+void freeEntries(EntryList *list);
+
+/* Trier les entrées par tri à bulles sans utiliser strcmp */
+void bubbleSort(EntryList *list);
+
+/* Fonction pour comparer deux chaînes sans utiliser strcmp */
+int compareEntries(const char *a, const char *b);
+
+/* Fonction pour afficher le contenu d'un répertoire */
+void listDirectory(const char *path);
+
+int main(int argc, char *argv[]);
+
+/* Fonction pour comparer deux chaînes sans utiliser strcmp */
+int compareEntries(const char *a, const char *b)
+{
+    while (*a != '\0' && *b != '\0')
+    {
+        char charA = (*a >= 'A' && *a <= 'Z') ? (*a + 32) : *a; // Convertir en minuscule si nécessaire
+        char charB = (*b >= 'A' && *b <= 'Z') ? (*b + 32) : *b;
+
+        if (charA < charB)
+            return -1;
+        else if (charA > charB)
+            return 1;
+
+        a++;
+        b++;
+    }
+
+    if (*a == '\0' && *b != '\0') // a est plus court que b, donc a < b
+        return -1;
+    else if (*a != '\0' && *b == '\0') // a est plus long que b, donc a > b
+        return 1;
+
+    return 0; // Les chaînes sont égales
+}
+
+/* Initialiser la liste d'entrées */
 void initializeList(EntryList *list)
 {
     list->count = 0;
@@ -83,30 +128,6 @@ void bubbleSort(EntryList *list)
     }
 }
 
-/* Fonction pour comparer deux chaînes sans utiliser strcmp */
-int compareEntries(const char *a, const char *b)
-{
-    while (*a != '\0' && *b != '\0')
-    {
-        char charA = (*a >= 'A' && *a <= 'Z') ? (*a + 32) : *a;
-        char charB = (*b >= 'A' && *b <= 'Z') ? (*b + 32) : *b;
-
-        if (charA < charB)
-            return -1;
-        else if (charA > charB)
-            return 1;
-
-        a++;
-        b++;
-    }
-
-    if (*a == '\0' && *b != '\0')
-        return -1;
-    else if (*a != '\0' && *b == '\0')
-        return 1;
-
-    return 0;
-
 /* Fonction pour afficher le contenu d'un répertoire */
 void listDirectory(const char *path)
 {
@@ -153,22 +174,26 @@ void listDirectory(const char *path)
     freeEntries(&entries);
 }
 
+/* Fonction principale */
 int main(int argc, char *argv[])
 {
     int i;
 
     if (argc < 2)
     {
+        // Pas d'arguments, lister le répertoire courant
         listDirectory(".");
     }
     else
     {
+        // Lister chaque répertoire ou fichier spécifié en argument
         for (i = 1; i < argc; i++)
         {
             struct stat path_stat;
 
             if (lstat(argv[i], &path_stat) == -1)
             {
+                // Gérer les erreurs d'accès au répertoire ou au fichier
                 fprintf(stderr, "%s: cannot access %s: ", argv[0], argv[i]);
                 perror("");
             }
@@ -176,11 +201,13 @@ int main(int argc, char *argv[])
             {
                 if (S_ISDIR(path_stat.st_mode))
                 {
+                    // Le chemin spécifié est un répertoire, afficher le contenu du répertoire
                     printf("%s:\n", argv[i]);
                     listDirectory(argv[i]);
                 }
                 else
                 {
+                    // Le chemin spécifié est un fichier, afficher le nom du fichier
                     printf("%s\n", argv[i]);
                 }
             }
