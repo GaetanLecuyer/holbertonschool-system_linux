e526d32bcf629206f84325b89725daf3399f4151
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-12-15 16:46:09 +0100

    corection

--- getline/_getline.h
+++ getline/_getline.h
@@ -24,11 +24,11 @@
  */
 typedef struct fd
 {
-	int fd;
-	char *buf;
-	size_t i;
-	size_t len;
-	struct fd *next;
+    int fd;
+    char *buf;
+    size_t i;
+    size_t len;
+    struct fd *next;
 } FdBuf;
 
 #define READ_SIZE 1024
@@ -38,9 +38,5 @@ char *read_buf(FdBuf *fb);
 FdBuf *get_fdbuf(FdBuf *head, const int fd);
 char *_strchr(char *s, char c, ssize_t size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *__getline(const int fd);
-char *__strchr(char *s, char c, ssize_t size);
-
-
 
 #endif
