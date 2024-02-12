1dfc840ed49100035f641c23c61d05e6c92c77e8
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2024-02-12 15:22:27 +0100

    test

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,37 +2,33 @@ section .text
     global asm_strcmp
 
 asm_strcmp:
-    ; Entrées : rdi - pointeur vers la première chaîne (s1)
-    ;           rsi - pointeur vers la deuxième chaîne (s2)
-    ; Sortie : rax - résultat entier de la comparaison des chaînes
-    ;                  0 si les chaînes sont égales
-    ;                  positif si s1 > s2
-    ;                  négatif si s1 < s2
+    ; Arguments:
+    ;   rdi: pointeur vers la première chaîne
+    ;   rsi: pointeur vers la deuxième chaîne
 
-    ; Boucle pour parcourir les chaînes
-.loop:
-    ; Charger les caractères actuels de s1 et s2
-    mov al, [rdi]
-    mov bl, [rsi]
+    ; Initialisation des registres
+    xor rax, rax  ; rax: résultat
+    xor rcx, rcx  ; rcx: compteur pour boucle
 
-    ; Comparer les caractères
-    cmp al, bl
+    ; Boucle de comparaison des caractères
+    cmp_loop:
+        ; Charger le caractère de la première chaîne dans al
+        mov al, [rdi + rcx]
+        ; Charger le caractère de la deuxième chaîne dans bl
+        mov bl, [rsi + rcx]
+        ; Comparer les caractères
+        cmp al, bl
+        ; Si les caractères sont différents, terminer la comparaison
+        jne cmp_done
+        ; Si les deux caractères sont nuls, terminer la comparaison
+        test al, al
+        jz cmp_done
+        ; Incrémenter le compteur et continuer la boucle
+        inc rcx
+        jmp cmp_loop
 
-    ; Si les caractères ne sont pas égaux ou si on atteint la fin de l'une des chaînes, sortir de la boucle
-    jne .end
-    ; Si on atteint la fin de l'une des chaînes, sortir de la boucle
-    cmp al, 0
-    je .end
-
-    ; Passer au caractère suivant dans chaque chaîne
-    inc rdi
-    inc rsi
-
-    ; Continuer la boucle
-    jmp .loop
-
-.end:
-    ; Calculer le résultat final en fonction de la dernière comparaison
-    sub al, bl
-
-    ret
+    cmp_done:
+        ; Soustraire les caractères pour obtenir le résultat
+        sub al, bl
+        ; Retourner le résultat
+        ret
